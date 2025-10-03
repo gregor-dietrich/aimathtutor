1a4440d93af9138cd6940bb8884834c7044b22ab
Gregor Dietrich <gregor@dietrich.lan>
2025-10-03 20:31:37 +0200

    Remove + Ignore generated frontend files

--- src/main/frontend/generated/vaadin.ts
+++
@@ -1,8 +0,0 @@
-import './vaadin-featureflags.js';
-
-import './index';
-
-import './vaadin-react.js';
-import './theme-starter-theme.global.generated.js';
-import { applyTheme } from './theme.js';
-applyTheme(document);
